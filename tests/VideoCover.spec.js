d3674dfcd2f7dda2ec4bd7ea5d5ea315b868936f
Tobias Kloht <tobias.kloht@gmail.com>
2016-06-30 01:09:09 +0200

    use multinline strings

--- tests/VideoCover.spec.js
+++ tests/VideoCover.spec.js
@@ -35,22 +35,26 @@ describe('VideoCover', () => {
       {
         name: 'Trident',
         needsFallback: true,
-        value: 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)',
+        value: `Mozilla/5.0(compatible; MSIE 10.0;
+          WindowsNT 6.2; Win64; x64; Trident/6.0)`,
       },
       {
         name: 'Edge',
         needsFallback: true,
-        value: 'Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36 Edge/12.0',
+        value: `Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36
+          (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36 Edge/12.0`,
       },
       {
         name: 'Safari',
         needsFallback: false,
-        value: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/601.5.17 (KHTML, like Gecko) Version/9.1 Safari/601.5.17',
+        value: `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4)
+          AppleWebKit/601.5.17 (KHTML, like Gecko) Version/9.1 Safari/601.5.17`,
       },
       {
         name: 'Chrome',
         needsFallback: false,
-        value: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.86 Safari/537.36',
+        value: `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4)
+          AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.86 Safari/537.36`,
       },
       {
         name: 'Firefox',
@@ -66,7 +70,9 @@ describe('VideoCover', () => {
 
     /* generate tests for some known userAgents */
     userAgents.forEach(({ needsFallback, name, value }) => {
-      it(`should ${needsFallback ? '' : 'NOT'} render Fallback if ${name} userAgent is used`, () => {
+      it(`should ${
+        needsFallback ? '' : 'NOT'
+        } render Fallback if ${name} userAgent is used`, () => {
         setUserAgent(value);
         const wrapper = shallow(<VideoCover />);
         if (needsFallback) {
@@ -76,6 +82,5 @@ describe('VideoCover', () => {
         }
       });
     });
-    /* eslint-enable max-len */
   });
 });
