a351d2ad947a7ce6800664b3cd3c4749af57ee6e
Tobias Kloht <tobias.kloht@gmail.com>
2016-07-07 12:52:28 +0200

    update public path

--- webpack.config.examples_dist.js
+++ webpack.config.examples_dist.js
@@ -44,7 +44,7 @@ const CONFIG_EXAMPLE_DIST = {
   },
   output: {
     path: path.resolve(__dirname, 'dist_examples'),
-    publicPath: '/react-video-cover',
+    publicPath: '/',
     filename: 'bundle.js',
   },
   plugins: [
