dbce7580de4e904a49f4e86b73c81028caf96004
Tobias Kloht <tobias.kloht@gmail.com>
2016-08-12 15:10:25 +0200

    add home route and component

--- examples/routes.js
+++ examples/routes.js
@@ -6,11 +6,12 @@ import NoMatch from './NoMatch';
 import MinimalExample from './MinimalExample';
 import ResizableExample from './ResizableCoverExample';
 import BackgroundExample from './BackgroundCoverExample';
+import Home from './Home';
 
 export default (
   <Route>
     <Route path="/" component={App}>
-      <IndexRoute component={ResizableExample} />
+      <IndexRoute component={Home} />
       <Route path="resizable" component={ResizableExample} />
       <Route path="background" component={BackgroundExample} />
       <Route path="minimal" component={MinimalExample} />
