f36d138eabcb9ab082829e352e0a171f48cf0900
Tobias Kloht <tobias.kloht@gmail.com>
2017-06-19 14:02:14 +0200

    use prop-types package

--- examples/NavItem.js
+++ examples/NavItem.js
@@ -1,5 +1,7 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+
 import css from './NavBar.css';
 
 class Navitem extends Component {
