b9ea4f3bf139f7dc9f9e42243eda82647e721cd9
Tobias Kloht <mail@kloht.com>
2018-04-02 14:57:41 +0200

    adjust test setup
    after component scripts update: now using jest instead of enzyme
    -> required file-structure changes, some updates to the test mostly
    because now using jest expect instead of chai assertions.
    
    test which require mocked useragent are not working currently, this
    should be fixed but i couldn't figure out how to do it in jest yet.

---
+++ __mocks__/react-intl.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Intl = require.requireActual('react-intl');
+
+// Here goes intl context injected into component, feel free to extend
+const intl = {
+  formatMessage: ({defaultMessage}) => defaultMessage
+};
+
+Intl.injectIntl = (Node) => {
+  const renderWrapped = props => <Node {...props} intl={intl} />;
+  renderWrapped.displayName = Node.displayName
+    || Node.name
+    || 'Component';
+  return renderWrapped;
+};
+
+module.exports = Intl;
